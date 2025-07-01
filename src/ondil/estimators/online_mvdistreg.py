6a607de1ed169ca528fcfd0fec33935c403ba328
tatris <christian-schulz@outlook.de>
2025-07-01 09:38:14 +0200

    test

--- src/ondil/estimators/online_mvdistreg.py
+++ src/ondil/estimators/online_mvdistreg.py
@@ -35,7 +35,6 @@ try:
 except ImportError:
     HAS_POLARS = False
 
-# test
 
 def indices_along_diagonal(D: int) -> List:
     """This functions generates a list of indices that will go along
@@ -55,6 +54,8 @@ def indices_along_diagonal(D: int) -> List:
             K.append((K[-1] + j))
     return K
 
+
+# test
 
 @nb.jit(
     [
